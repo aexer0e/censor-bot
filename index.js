83060ab65ef42ea2300caaabf0d2e9fceea2c06e
aexer0e <59780229+aexer0e@users.noreply.github.com>
2020-01-12 02:07:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+bot.on('message', (message) => {
+
+    if(message.content.split(" ").join("").toLowerCase().includes('razan') && message.author.id != '321317901086883840') {
+        message.delete();
+        message.channel.send('Fuck you, '+message.author+'.').then(d_msg => { d_msg.delete(4000); });
+    };
+
+    if(message.content.split(" ").join("").toLowerCase().includes('razan') && message.author.id == '321317901086883840') {
+        message.delete();
+        message.channel.send('I am sorry senpai '+message.author+', but you have programmed me to delete that message.').then(d_msg => { d_msg.delete(4000); });
+    };
+});
+
+
+
+bot.login('NjY1NjI5MTA1ODAyMzEzNzU5.XhoeWA.xqQ4P6IshvnGCFqD2iAI6I5-5bE');
